93f7a7729b92235afa405e49bf5906559746f760
SagittariusA11 <manashkumar1216@gmail.com>
2024-09-20 03:16:31 +0530

    vercel updated

--- backend/index.js
+++ backend/index.js
@@ -167,15 +167,15 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
-// app.listen(port, () => {
-//   connect();
-//   console.log("Server running on 3000");
-// });
+app.listen(port, () => {
+  connect();
+  console.log("Server running on 3000");
+});
 
 module.exports = app;
