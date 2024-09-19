3b72d087dd16d9b8c0fab2bb99dde91c9823315b
SagittariusA11 <manashkumar1216@gmail.com>
2024-09-20 02:39:51 +0530

    vercel updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-const App = () => {
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/hello`)
+      .then((response) => response.json())
+      .then((data) => setGreeting(data.greeting))
+      .catch((error) => console.error('Error fetching greeting:', error));
+  }, []);
+
   return (
-    <div>Hello World</div>
-  )
+    <div className="App">
+      <h1>{greeting}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
