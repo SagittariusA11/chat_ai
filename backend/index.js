d539a4405bbe58c62be9b2667e41707c63c15f73
SagittariusA11 <manashkumar1216@gmail.com>
2024-09-20 16:44:25 +0530

    vercel updated

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization'
   })
 );
 
